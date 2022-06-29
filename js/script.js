6f59d624bb43fc08827ffc6dc422588c961decb1
feddericahu99 <federica.hu339@gmail.com>
2022-06-29 16:57:03 +0200

    added first function

--- js/script.js
+++ js/script.js
@@ -9,25 +9,73 @@
     4- inverto l'ordine della parola.
     5- vedo se il valore e tipo sono uguali.
 */
-//prompt per l'utente
-const userWord = prompt('Scegli la parola di cui vuoi sapere se è palindroma:');
+// //prompt per l'utente
+// const userWord = prompt('Scegli la parola di cui vuoi sapere se è palindroma:');
 
-//funzione 
-function isPalindrome (userWord) {
-    let answer = false;
-    let word= '';
-//inverto lettere
-    for (let i = userWord.length -1; i >= 0; i-- ) {
-        word += userWord[i];
-    } 
-//se la parola del promp è uguale al nostro risultato allora = true
-    if (word == userWord) {
-        answer = true;
-    }
+// //funzione 
+// function isPalindrome (userWord) {
+//     let answer = false;
+//     let word= '';
+// //inverto lettere
+//     for (let i = userWord.length -1; i >= 0; i-- ) {
+//         word += userWord[i];
+//     } 
+// //se la parola del promp è uguale al nostro risultato allora = true
+//     if (word == userWord) {
+//         answer = true;
+//     }
 
-    return answer;
+//     return answer;
+// }
+
+// const answer = isPalindrome(userWord); //obbligatorio per vedere il risultato della function
+// console.log(answer)
+
+
+/*
+ Consegna:
+    L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+    Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+    Sommiamo i due numeri.
+    Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
+    Dichiariamo chi ha vinto.
+ Esecuzione: 
+    1- prompt per decidere pari o dispari.
+    2- prompt per scelta del numero.
+    3- genero un numero random con function.
+    4- sommo i due numeri.
+    5- creo funzione per capire se è pari o dispari.
+    6- dichiaro il vincitore.
+*/
+
+// prompt per pari o dispari
+const choiseOfOddEven = prompt('scegli tra "pari" o "dispari"').trim();
+//controllo
+if (choiseOfOddEven != 'pari' || choiseOfOddEven != 'dispari') {
+    console.error('scegli tra "pari" e "dispari"');
+}
+console.log(choiseOfOddEven)
+
+//prompt per numero tra 1 e 5 
+const choiseOfNumber = parseInt(prompt('scegli un numero tra 1 e 5'));
+//controllo
+if (choiseOfNumber < 1 || choiseOfNumber > 5 ) {
+    console.error('scegli un numero compreso tra 1 e 5');
+    location.reload();
+}
+console.log(choiseOfNumber)
+
+// function per numero casuale
+function getNumber ( min = 1 , max = 5) {
+    randomNumber ='';
+    parseInt(randomNumber);
+    randomNumber += Math.floor(Math.random() * ( max - min )) + min;
+
+    return randomNumber;
 }
 
-const answer = isPalindrome(userWord); //obbligatorio per vedere il risultato della function
-console.log(answer)
+const botNumber = getNumber( min = 1 , max = 5);
+console.log(randomNumber)
 
+const sum= randomNumber + choiseOfNumber;
+console.log(sum);
