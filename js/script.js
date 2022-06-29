6ac788e92d5b3e332cd225fb80e079bdc9bdb585
feddericahu99 <federica.hu339@gmail.com>
2022-06-29 15:57:48 +0200

    commented and fixed

--- js/script.js
+++ js/script.js
@@ -9,21 +9,22 @@
     4- inverto l'ordine della parola.
     5- vedo se il valore e tipo sono uguali.
 */
-
+//propmt per l'utente
 const userWord = prompt('Scegli la parola di cui vuoi sapere se è palindroma:');
 
+//funzione 
 function isPalindrome (userWord) {
     let answer = false;
-
-    for ( i = userWord = userWord.lenght -1; i > 0; i-- ) {
-        answer += userWord[i]
+    let word= '';
+//inverto lettere
+     for (let i = userWord.lenght -1; i >= 0; i-- ) {
+        word += userWord[i];
     } 
-
-    if (answer === userWord) {
-        answer = true
+//se la parola del promp è uguale al nostro risultato allora = true
+    if (word == userWord) {
+        answer = true;
     }
 
     return answer;
 }
 
-isPalindrome();
